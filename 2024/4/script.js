9039910ceb491304f85c7a7383b2c61df5741eb9
lukas.bauer <lukas.bauer@yazio.com>
2024-12-04 22:14:50 +0100

    make it shorter

--- 2024/4/script.js
+++ 2024/4/script.js
@@ -2,55 +2,32 @@ const aocData = process.env.AOC_INPUT;
 
 const findPhrase = "XMAS";
 
-const matrix = aocData
-  .split(/\r?\n/)
-  .filter((row) => row.trim() !== "")
-  .map((row) => row.split(""));
-
-const maxY = matrix.length - 1;
-const maxX = matrix[0].length - 1;
+const dat = aocData.split(/\r?\n/).map((row) => row.split(""));
 
 let count = 0;
-for (let y = 0; y < matrix.length; y++) {
-  for (let x = 0; x < matrix[y].length; x++) {
-    if (matrix[y][x] === findPhrase[0]) {
-      const remainingPhrase = findPhrase.substring(1);
+dat.map((row, y) =>
+  row.map((_, x) => {
+    if (dat[y][x] === findPhrase[0]) {
+      [-1, 0, 1]
+        .flatMap((xx) => [-1, 0, 1].map((yy) => ({ xx, yy })))
+        .map((direction) => {
+          (function next({ x, y }, { xx, yy }, phrase) {
+            x = x + xx;
+            y = y + yy;
 
-      const directions = [-1, 0, 1].flatMap((x) =>
-        [-1, 0, 1].map((y) => ({ x, y }))
-      );
+            if (x < 0 || x > dat[0].length - 1 || y < 0 || y > dat.length - 1)
+              return false;
 
-      for (const direction of directions) {
-        const foundPhrase = foundNextCharacter(
-          { x, y },
-          direction,
-          remainingPhrase
-        );
-        if (foundPhrase) count++;
-      }
+            if (dat[y][x] === phrase[0]) {
+              if (phrase.substring(1) === "") return true;
+              return next({ x, y }, { xx, yy }, phrase.substring(1));
+            }
+
+            return false;
+          })({ x, y }, direction, findPhrase.substring(1)) && count++;
+        });
     }
-  }
-}
+  })
+);
 
 console.log(`Found ${count} times '${findPhrase}'`);
-
-function foundNextCharacter(pos, direction, phrase) {
-  pos = { x: pos.x + direction.x, y: pos.y + direction.y };
-
-  if (pos.x < 0 || pos.x > maxX || pos.y < 0 || pos.y > maxY) return false;
-
-  if (matrix[pos.y][pos.x] === phrase[0]) {
-    const remainingPhrase = phrase.substring(1);
-
-    if (remainingPhrase === "") return true;
-
-    return foundNextCharacter(
-      { x: pos.x, y: pos.y },
-      direction,
-      phrase.substring(1)
-    );
-  }
-
-  return false;
-}
-
